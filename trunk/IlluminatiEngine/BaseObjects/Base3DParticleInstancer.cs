c120a17395f1b6436faf639b112b004d5681fd44
markneale <markneale@hotmail.com>
2012-10-22 11:44:56 +0000

    added object array to core library to remove direct BulletXNA dependency. prep work for more physics systems.

--- trunk/IlluminatiEngine/BaseObjects/Base3DParticleInstancer.cs
+++ trunk/IlluminatiEngine/BaseObjects/Base3DParticleInstancer.cs
@@ -1,11 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using BulletXNA.LinearMath;
 
 namespace IlluminatiEngine
 {
